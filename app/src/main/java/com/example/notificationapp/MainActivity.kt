2ec5f23b6ca01b5b025b9699eefdfadc3e24320e
Niraviman <niravimansingharoy71@gmail.com>
2023-10-21 12:00:37 +0600

    Added Message Category

--- app/src/main/java/com/example/notificationapp/MainActivity.kt
+++ app/src/main/java/com/example/notificationapp/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.notificationapp
 
 import android.app.NotificationManager
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import com.example.notificationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding:ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
@@ -16,21 +17,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.highBtn.setOnClickListener {
-            val notification = NotificationCompat.Builder(this,App().CHANNEL_ID1)
+            val notification = NotificationCompat.Builder(this, App().CHANNEL_ID1)
             notification.setContentTitle(binding.titleET.text.toString())
             notification.setContentTitle(binding.contentET.text.toString())
             notification.setSmallIcon(R.drawable.high)
-                notification.setPriority(NotificationCompat.PRIORITY_HIGH)
+            notification.setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.MAGENTA)
+                .addAction(R.drawable.high, "Back", null)
+                .addAction(R.drawable.high, "Play", null)
+                .addAction(R.drawable.high, "Next", null)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
                 .build()
 
             val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            manager.notify(1,notification.build())
+            manager.notify(1, notification.build())
 
         }
 
         binding.lowBtn.setOnClickListener {
 
-            val notification = NotificationCompat.Builder(this,App().CHANNEL_ID2)
+            val notification = NotificationCompat.Builder(this, App().CHANNEL_ID2)
             notification.setContentTitle(binding.titleET.text.toString())
             notification.setContentTitle(binding.contentET.text.toString())
             notification.setSmallIcon(R.drawable.low)
@@ -38,7 +46,7 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
             val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            manager.notify(2,notification.build())
+            manager.notify(2, notification.build())
 
         }
     }
