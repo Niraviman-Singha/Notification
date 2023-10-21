eaf5e33e2a10d92e08976f23ee35c7ca30e3628b
Niraviman <niravimansingharoy71@gmail.com>
2023-10-21 10:33:47 +0600

    Showing Notification

--- app/src/main/java/com/example/notificationapp/MainActivity.kt
+++ app/src/main/java/com/example/notificationapp/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.notificationapp
 
+import android.app.NotificationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import com.example.notificationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding:ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.highBtn.setOnClickListener {
+            val notification = NotificationCompat.Builder(this,App().CHANNEL_ID1)
+            notification.setContentTitle(binding.titleET.text.toString())
+            notification.setContentTitle(binding.contentET.text.toString())
+            notification.setSmallIcon(R.drawable.high)
+                .build()
+
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.notify(1,notification.build())
+
+        }
+
+        binding.lowBtn.setOnClickListener {
+
+            val notification = NotificationCompat.Builder(this,App().CHANNEL_ID2)
+            notification.setContentTitle(binding.titleET.text.toString())
+            notification.setContentTitle(binding.contentET.text.toString())
+            notification.setSmallIcon(R.drawable.high)
+                .build()
+
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.notify(2,notification.build())
+
+        }
     }
 }
